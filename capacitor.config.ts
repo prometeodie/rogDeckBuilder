776ddf992efce25f62154125ae706445c24fe3d5
Franco D Rodriguez <franco.d.r1992@gmail.com>
2025-11-17 22:19:33 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rogDeckBuilder',
+  webDir: 'www'
+};
+
+export default config;
