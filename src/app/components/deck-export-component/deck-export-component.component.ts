35c3162f74b777452432092064cf90ccdefa15c5
Franco D Rodriguez <franco.d.r1992@gmail.com>
2025-12-24 10:44:12 -0300

    amount of card bug fixed

--- src/app/components/deck-export-component/deck-export-component.component.ts
+++ src/app/components/deck-export-component/deck-export-component.component.ts
@@ -1,38 +1,56 @@
-import { CommonModule, DatePipe } from '@angular/common';
-import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 import { UserIdentityData } from 'src/app/interfaces/user.interface';
 import { User } from 'src/app/services/user';
-import { RogLogoComponent } from "../rog-logo/rog-logo.component";
+import { RogLogoComponent } from '../rog-logo/rog-logo.component';
 import { ExportCard } from 'src/app/interfaces/export.card.interface';
 
 @Component({
   selector: 'deck-export',
-  templateUrl: './deck-export-component.component.html',
-  styleUrls: ['./deck-export-component.component.scss'],
   standalone: true,
-  imports: [CommonModule, RogLogoComponent]
+  imports: [CommonModule, RogLogoComponent],
+  templateUrl: './deck-export-component.component.html',
+  styleUrls: ['./deck-export-component.component.scss']
 })
-export class DeckExportComponentComponent  implements OnInit, OnChanges {
+export class DeckExportComponentComponent implements OnInit, OnChanges {
+
   @Input() mainDeck: ExportCard[] = [];
   @Input() sideDeck: ExportCard[] = [];
 
-  private userService = inject(User)
+  private userService = inject(User);
+
   public user!: UserIdentityData;
   public today = new Date();
 
-  ngOnInit() {
+  public mainDeckTotalCards = 0;
+  public sideDeckTotalCards = 0;
+
+  ngOnInit(): void {
     this.userService.getUserIdentity().then(user => {
-    this.user = user;
-  });
+      this.user = user;
+    });
   }
 
-   ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['mainDeck']?.currentValue) {
       this.mainDeck = [...this.mainDeck].sort((a, b) => b.qty - a.qty);
+      this.mainDeckTotalCards = this.calculateTotal(this.mainDeck);
     }
 
     if (changes['sideDeck']?.currentValue) {
       this.sideDeck = [...this.sideDeck].sort((a, b) => b.qty - a.qty);
+      this.sideDeckTotalCards = this.calculateTotal(this.sideDeck);
     }
+  }
+
+  private calculateTotal(deck: ExportCard[]): number {
+    return deck.reduce((total, card) => total + card.qty, 0);
   }
 }
